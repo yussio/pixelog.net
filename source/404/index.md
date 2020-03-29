98ef52031e83d2bc6b855c2c2e12d471b6dded96
onissyuta <fateseeker7@gmail.com>
2020-03-29 16:32:25 +0900

    テーマ：404ページ 愛生会病院風に

--- source/404/index.md
+++ source/404/index.md
@@ -5,6 +5,16 @@ js: true
 sitemap: false
 ---
 
+<p style="line-height: 60px;text-align:center;border: 6px solid #00efff;background:#000;animation: rainbow 3s ease-out -3s infinite alternate;">
+  <span style="animation-delay:.5s">Sorry, this page is Japanese only!</span>
+</p>
+
+<p class="marquee-anim" style="border: 4px solid #ff0000;background-color: #0348fb;color: #ffff00;">
+  <span>ページが見つかりませんでした！！！</span>
+</p>
+
+<p style="color:#0348fb">このページを見つけた人は、Twiｉｔer(<a href="https://twitter.com/yussious">@yussious</a>)まで報告してください！！<span class="shake" style="color: red">読み逃げは禁止です！！！</span></p>
+
 <div id="console-wrapper">
     <div id="console">
         <p>> ERROR! 未知のページを検知しました。</p>
@@ -19,4 +29,10 @@ sitemap: false
     </div>
 </div>
 
-[トップへ戻る](/)
+<p>
+↓↓このホームページは以下の同盟に参加しています。↓↓
+<br><br>
+<a href="http://m-pe.tv/u/page.php?uid=16811&id=1&guid=on"><img src="shiryoku.jpg" alt="視力回復同盟"></a>
+<a href="http://web.archive.org/web/20110728135322/http://trick.kill.jp/union/kie.htm"><img src="kie.gif" alt="消失同盟"></a>
+</p>
+<a href="/"><img src="top.gif" alt="トップへ戻る"></a>
--- themes/pixelog/source/404/script.js
+++ themes/pixelog/source/404/script.js
@@ -21,3 +21,54 @@ function displayConsole(elmId) {
 }
 
 displayConsole("console");
+
+
+
+//星を作る関数。n は星の個数。多いほど星が多く振ります。
+function starMaker(n) {
+    var star = document.createElement("div");
+    star.className = "star";
+    star.textContent = "★";
+    for(var i = 0; i < n; i++) {
+        starSet(star);
+    }
+}
+
+//星のセッティングをする関数。
+function starSet(clone) {
+    var starClone = clone.cloneNode(true);
+    var starStyle = starClone.style;
+
+    //星の位置（left）、アニメーションの遅延時間（animation-delay）、サイズ（font-size）をランダムで指定
+    starStyle.left = 100 * Math.random() + "%";
+    starStyle.animationDelay = 8 * Math.random() + "s";
+    starStyle.fontSize = ~~(50 * Math.random() + 20) + "px";
+    document.body.appendChild(starClone);
+
+    //星一つのアニメーションが終わったら新しい星を生成
+    starClone.addEventListener("animationend", function() {
+        this.parentNode.removeChild(this);
+        var star = document.createElement("div");
+        star.className = "star";
+        star.textContent = "★";
+        starSet(star);
+    }, false)
+}
+
+//使用例。星を50個ふらせます。
+starMaker(50);
+
+
+(function() {
+    var doc = document;
+    var body = doc.body;
+    var stalker = doc.createElement("div");
+    stalker.id = "stalker";
+    stalker.innerHTML = '<img src="icecream.png" alt="マウスストーカー" width="70" height="70">'; //表示させる要素 画像であれば img 要素を入れる
+    body.appendChild(stalker);
+
+    body.addEventListener("mousemove", function(e) {
+          stalker.style.left = e.clientX - 35 + "px"; //35 は要素の長さの半分
+          stalker.style.top = e.clientY - 80 + "px"; //80 は縦の位置 各自調整
+    }, false);
+})()
