ba4abeb1a0d4058232e4f3a7218012072055f271
onissyuta <fateseeker7@gmail.com>
2020-04-17 12:12:09 +0900

    :fire: コンテキストメニュー js削除

--- themes/pixelog/source/js/script.js
+++ themes/pixelog/source/js/script.js
@@ -72,12 +72,4 @@
     lazyLoad("https://www.googletagmanager.com/gtag/js?id=UA-68420269-3", "src");
     lazyLoad("window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag(\'js\', new Date());gtag(\'config\', \'UA-68420269-3\');", "inline");
     lazyLoad("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", "ad");
-
-
-    const btn = document.getElementById('dropdown__btn');
-    if(btn) {
-      btn.addEventListener('click', function(){
-        this.classList.toggle('is-open');
-      });
-    }
 }());
