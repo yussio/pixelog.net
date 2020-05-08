f845400f041cd884cf96d3126ee4a090330e6595
onissyuta <fateseeker7@gmail.com>
2020-05-08 15:48:14 +0900

    :fire: ダークモードスイッチ廃止

--- themes/pixelog/source/js/script.js
+++ themes/pixelog/source/js/script.js
@@ -1,49 +1,4 @@
 (function () {
-  const toggle_switch = document.getElementById('toggle-dark');
-  const isDark = window.matchMedia('(prefers-color-scheme: dark)');
-  const htmlElement = document.documentElement;
-  const keyLocalStorage = 'theme';
-  const localTheme = sessionStorage.getItem(keyLocalStorage);
-
-  if(localTheme) {
-    changeMode(localTheme);
-  } else if(isDark.matches) {
-    changeMode('dark');
-  }
-
-  toggle_switch.addEventListener('change', () => {
-    if(toggle_switch.checked){
-      changeMode('dark', 'set');
-    } else {
-      changeMode('light', 'set');
-    }
-  });
-  isDark.addListener(e => {
-    if (e.matches) {
-      changeMode('dark', 'remove');
-    } else {
-      changeMode('light', 'remove');
-    }
-  });
-
-  function changeMode(mode, storage){
-    if(mode === 'dark') {
-      htmlElement.dataset.mode = mode;
-      toggle_switch.checked = true;
-
-    } else if(mode === 'light') {
-      delete htmlElement.dataset.mode;
-      toggle_switch.checked = false;
-    }
-    if(storage === 'set'){
-      sessionStorage.setItem(keyLocalStorage, mode);
-    } else if(storage === 'remove') {
-      sessionStorage.removeItem(keyLocalStorage);
-    }
-  }
-}());
-
-(function () {
   function lazyLoad(scriptSrc, type) {
     let scrollFirstTime = 1;
     window.addEventListener("scroll", oneTimeFunction, false);
