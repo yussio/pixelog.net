9ddfbd9f6305d5b555ebd0bfcb000c3871900386
yussy <fateseeker7@gmail.com>
2020-11-15 15:59:18 +0900

    :fire: footnotesスクリプト除去

--- themes/hexo-theme-visual/scripts/footnotes.js
+++
@@ -1,4 +0,0 @@
-hexo.extend.filter.register('after_post_render', function(data){
-  data.content = data.content.replace('<hr class="footnotes-sep">', '').replace('<section class="footnotes">', '').replace('</li></ol></section>', '</li></ol>');
-  return data;
-});
